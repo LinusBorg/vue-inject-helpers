291e4fd21a8ae6bafbc4a348e0f6baf4e73e027c
Thorsten <t.luenborg@googlemail.com>
2018-09-13 23:20:10 +0200

    add bundle analyzer

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 process.env.VUE_APP_VERSION = require('./package.json').version
 module.exports = {
   lintOnSave: false,
+  chainWebpack: config => {
+    if (process.env.V_APP_ANALYZE) {
+      const Analyzer = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+      config.plugin('bundleAnalyzer').use(Analyzer, [
+        {
+          analyzerMode: 'static',
+        },
+      ])
+    }
+  },
 }
