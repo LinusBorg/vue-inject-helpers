7138f8a4d4d206ffd3aa1e46c30994f7571b4df4
Thorsten <t.luenborg@googlemail.com>
2018-09-13 23:20:43 +0200

    set up lib build

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,27 @@
+const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
-  presets: ["@vue/app"]
-};
+  presets: [
+    !isProd
+      ? '@vue/app'
+      : [
+          '@babel/env',
+          {
+            modules: 'commonjs',
+            targets: '> 1%, not dead, not IE <=8',
+            useBuiltIns: false,
+          },
+        ],
+  ],
+  plugins: isProd
+    ? [
+        [
+          '@babel/plugin-transform-runtime',
+          {
+            polyfill: false,
+            helpers: false,
+            regenerator: false,
+          },
+        ],
+      ]
+    : undefined,
+}
