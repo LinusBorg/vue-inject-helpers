c526a0a7eaea4912698f365af681d04852b91bb7
Thorsten <t.luenborg@googlemail.com>
2018-09-17 23:13:08 +0200

    feat: make InjectProvider actually work.
    Required to be made into a creator function to apply `inject` option

--- tests/unit/providerComponent.spec.js
+++ tests/unit/providerComponent.spec.js
@@ -1,7 +1,24 @@
-// import { mount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
+import ProviderContainer from './resources/Provider'
 
 describe('The Provider Component', () => {
   it('works', () => {
-    // nothing so far
+    const wrapper = mount(ProviderContainer)
+
+    const span = wrapper.find('.test-span')
+    expect(span.text()).toBe('test message')
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('re-renders when reactive data is changed', () => {
+    const wrapper = mount(ProviderContainer)
+
+    wrapper.setData({
+      msg: 'new test message',
+    })
+
+    const span = wrapper.find('.test-span')
+    expect(span.text()).toBe('new test message')
+    expect(wrapper.html()).toMatchSnapshot()
   })
 })
---
+++ tests/unit/resources/Provider.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <InjectProvider>
+      <div slot-scope="{msg}">
+        <span class="test-span">{{msg}}</span>
+      </div>
+    </InjectProvider>
+  </div>
+</template>
+<script>
+import { createInjectProvider } from '#lib/InjectProvider'
+export default {
+  name: 'TestProvider',
+  components: {
+    InjectProvider: createInjectProvider('test'),
+  },
+  data: () => ({
+    msg: 'test message',
+  }),
+  provide() {
+    return {
+      test: this.$data,
+    }
+  },
+}
+</script>
