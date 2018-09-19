b672b06710d853bfb21961980e2d132810192ff9
Thorsten <t.luenborg@googlemail.com>
2018-09-19 20:03:21 +0200

    placeholder tests for slots

---
+++ tests/unit/hoc-slots.spec.js
@@ -0,0 +1,9 @@
+import { mount } from '@vue/test-utils'
+
+describe('HOC Behaviour with slots', () => {
+  it('by default, named slots are passed through correctly', () => {
+    mount // something
+  })
+
+  it('when updateSlotContext = false, all slot nodes show up in default slot', () => {})
+})
